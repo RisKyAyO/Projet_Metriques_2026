ab18808eabb938ec552c308c9aad8e4ba5717a57
RisKyAyO <85305876+RisKyAyO@users.noreply.github.com>
2026-04-08 09:43:52 +0000

    Implémentation de l'application métriques

--- app.py
+++ app.py
@@ -9,12 +9,36 @@ def hello_world():
 
 # Déposez votre code à partir d'ici :
 
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
 
+@app.route('/paris')
+def paris():
+    url = "https://api.open-meteo.com/v1/forecast"
+    params = {
+        "latitude": 48.8566,
+        "longitude": 2.3522,
+        "hourly": "temperature_2m",
+        "forecast_days": 1
+    }
+    response = requests.get(url, params=params)
+    data = response.json()
+    result = {
+        "time": data["hourly"]["time"],
+        "temperature_2m": data["hourly"]["temperature_2m"]
+    }
+    return jsonify(result)
 
+@app.route('/temperatures')
+def temperatures():
+    return render_template('temperatures.html')
 
-
+@app.route('/histogramme')
+def histogramme():
+    return render_template('histogramme.html')
 
 # Ne rien mettre après ce commentaire
-    
+
 if __name__ == "__main__":
-  app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
