41eee143d68399d8c094c746cc6ff47e71cfb392
RisKyAyO <85305876+RisKyAyO@users.noreply.github.com>
2026-04-08 10:02:49 +0000

    Ajout routes /rapport et /atelier

--- app.py
+++ app.py
@@ -30,13 +30,31 @@ def paris():
     }
     return jsonify(result)
 
-@app.route('/temperatures')
-def temperatures():
-    return render_template('temperatures.html')
+@app.route('/rapport')
+def rapport():
+    return render_template('graphique.html')
 
 @app.route('/histogramme')
 def histogramme():
     return render_template('histogramme.html')
+
+@app.route('/atelier')
+def atelier():
+    url = "https://api.open-meteo.com/v1/forecast"
+    params = {
+        "latitude": 48.8566,
+        "longitude": 2.3522,
+        "hourly": ["windspeed_10m", "precipitation"],
+        "forecast_days": 1
+    }
+    response = requests.get(url, params=params)
+    data = response.json()
+    result = {
+        "time": data["hourly"]["time"],
+        "windspeed_10m": data["hourly"]["windspeed_10m"],
+        "precipitation": data["hourly"]["precipitation"]
+    }
+    return jsonify(result)
 
 # Ne rien mettre après ce commentaire
 
